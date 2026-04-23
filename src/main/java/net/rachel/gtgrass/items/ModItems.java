9dc7efcfce6633d847874e34bbccfeca4a94b271
RachelNoName <177692971+RachelNoName@users.noreply.github.com>
2026-04-23 22:31:38 +0200

    first commit

---
+++ src/main/java/net/rachel/gtgrass/items/ModItems.java
@@ -0,0 +1,18 @@
+package net.rachel.gtgrass.items;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.rachel.gtgrass.GTGrass;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, GTGrass.MODID);
+
+    public static final RegistryObject<Item> GRASS_BUNDLE = ITEMS.register("grass_bundle", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
